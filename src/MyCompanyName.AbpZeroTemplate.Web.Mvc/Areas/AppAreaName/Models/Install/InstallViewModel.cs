c4c6afbc3cceecb38a63194fc4ebbcb2a2e79341
liuxing <liu1218xing@163.com>
2018-03-01 11:33:53 +0800

    first add commit

---
+++ src/MyCompanyName.AbpZeroTemplate.Web.Mvc/Areas/AppAreaName/Models/Install/InstallViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.Localization;
+using MyCompanyName.AbpZeroTemplate.Configuration.Host.Dto;
+using MyCompanyName.AbpZeroTemplate.Install.Dto;
+
+namespace MyCompanyName.AbpZeroTemplate.Web.Areas.AppAreaName.Models.Install
+{
+    public class InstallViewModel
+    {
+        public List<ApplicationLanguage> Languages { get; set; }
+
+        public AppSettingsJsonDto AppSettingsJson { get; set; }
+    }
+}
